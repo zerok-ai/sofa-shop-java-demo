dd93602a36ae99961d73e2ad336573f9fc7b2364
Mudit Krishna Mathur <muditkmathur@gmail.com>
2023-02-03 15:48:25 +0530

    Fixed the paths and kustomization

--- setup.sh
+++ setup.sh
@@ -3,19 +3,20 @@
 #     ./setup.sh [apply|delete]
 #  install/removes the deployment in specified Namespace.
 ### 
+scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
 DEFAULT_NAMESPACE='zerok-demoapp'
 APPLY_COMMAND='apply'
 DELETE_COMMAND='delete'
 COMMAND="${1:-$APPLY_COMMAND}"
-NAMESPACE="${2:-DEFAULT_NAMESPACE}"
+NAMESPACE="${2:-$DEFAULT_NAMESPACE}"
 
 if [[ "$COMMAND" == "$APPLY_COMMAND" ]]
 then
     kubectl create namespace $NAMESPACE
-    kubectl $COMMAND -n $NAMESPACE -k ./
-    sed -e "s/\${NAMESPACE}/$NAMESPACE/" order/k8s/app-configmap_template.yaml > order/k8s/app-configmap.yaml
-    sed -e "s/\${NAMESPACE}/$NAMESPACE/" inventory/k8s/app-configmap_template.yaml > inventory/k8s/app-configmap.yaml
+    kubectl $COMMAND -n $NAMESPACE -k ${scriptDir}/
+    sed -e "s/\${NAMESPACE}/$NAMESPACE/" ${scriptDir}/order/k8s/app-configmap_template.yaml > ${scriptDir}/order/k8s/app-configmap.yaml
+    sed -e "s/\${NAMESPACE}/$NAMESPACE/" ${scriptDir}/inventory/k8s/app-configmap_template.yaml > ${scriptDir}/inventory/k8s/app-configmap.yaml
 
 elif [[ "$COMMAND" == "$DELETE_COMMAND"  ]]
 then 
