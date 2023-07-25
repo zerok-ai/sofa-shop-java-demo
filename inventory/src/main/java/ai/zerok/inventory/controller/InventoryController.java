df1171166adbca8b2455fbd05ccaa3775aa0861f
avingoyal <avingoyal01+github@gmail.com>
2023-07-25 16:58:56 +0530

    increasing memory footprint

--- inventory/src/main/java/ai/zerok/inventory/controller/InventoryController.java
+++ inventory/src/main/java/ai/zerok/inventory/controller/InventoryController.java
@@ -4,11 +4,22 @@ import ai.zerok.inventory.model.InventoryDetailsResponse;
 import ai.zerok.inventory.model.InventoryRequest;
 import ai.zerok.inventory.model.InventoryRequestArr;
 import ai.zerok.inventory.service.InventoryService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,6 +28,8 @@ import java.util.List;
 public class InventoryController {
 
     private final InventoryService inventoryService;
+    private final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private final String JSONARRAY_STRING = "[{\"header\":{\"title\":\"Random JSON Data\",\"date\":\"2023-07-25\",\"author\":\"ChatGPT\"},\"content\":{\"section1\":{\"heading\":\"Introduction\",\"text\":\"This is a randomly generated JSON file with a size of approximately 5KB. It's intended for demonstration and testing purposes only.\"},\"section2\":{\"heading\":\"Data\",\"items\":[{\"id\":1,\"name\":\"Item 1\",\"description\":\"This is the first item in the data list.\",\"quantity\":10,\"price\":19.99},{\"id\":2,\"name\":\"Item 2\",\"description\":\"This is the second item in the data list.\",\"quantity\":5,\"price\":24.99},{\"id\":3,\"name\":\"Item 3\",\"description\":\"This is the third item in the data list.\",\"quantity\":3,\"price\":34.99}]},\"section3\":{\"heading\":\"Users\",\"users\":[{\"id\":101,\"name\":\"John Doe\",\"email\":\"john@example.com\",\"age\":30},{\"id\":102,\"name\":\"Jane Smith\",\"email\":\"jane@example.com\",\"age\":25},{\"id\":103,\"name\":\"Mike Johnson\",\"email\":\"mike@example.com\",\"age\":40},{\"id\":101,\"name\":\"John Doe\",\"email\":\"john@example.com\",\"age\":30},{\"id\":102,\"name\":\"Jane Smith\",\"email\":\"jane@example.com\",\"age\":25},{\"id\":103,\"name\":\"Mike Johnson\",\"email\":\"mike@example.com\",\"age\":40}]}}},{\"header\":{\"title\":\"Random JSON Data\",\"date\":\"2023-07-25\",\"author\":\"ChatGPT\"},\"content\":{\"section1\":{\"heading\":\"Introduction\",\"text\":\"This is a randomly generated JSON file with a size of approximately 5KB. It's intended for demonstration and testing purposes only.\"},\"section2\":{\"heading\":\"Data\",\"items\":[{\"id\":1,\"name\":\"Item 1\",\"description\":\"This is the first item in the data list.\",\"quantity\":10,\"price\":19.99},{\"id\":2,\"name\":\"Item 2\",\"description\":\"This is the second item in the data list.\",\"quantity\":5,\"price\":24.99},{\"id\":3,\"name\":\"Item 3\",\"description\":\"This is the third item in the data list.\",\"quantity\":3,\"price\":34.99}]},\"section3\":{\"heading\":\"Users\",\"users\":[{\"id\":101,\"name\":\"John Doe\",\"email\":\"john@example.com\",\"age\":30},{\"id\":102,\"name\":\"Jane Smith\",\"email\":\"jane@example.com\",\"age\":25},{\"id\":103,\"name\":\"Mike Johnson\",\"email\":\"mike@example.com\",\"age\":40},{\"id\":101,\"name\":\"John Doe\",\"email\":\"john@example.com\",\"age\":30},{\"id\":102,\"name\":\"Jane Smith\",\"email\":\"jane@example.com\",\"age\":25},{\"id\":103,\"name\":\"Mike Johnson\",\"email\":\"mike@example.com\",\"age\":40}]}}},{\"header\":{\"title\":\"Random JSON Data\",\"date\":\"2023-07-25\",\"author\":\"ChatGPT\"},\"content\":{\"section1\":{\"heading\":\"Introduction\",\"text\":\"This is a randomly generated JSON file with a size of approximately 5KB. It's intended for demonstration and testing purposes only.\"},\"section2\":{\"heading\":\"Data\",\"items\":[{\"id\":1,\"name\":\"Item 1\",\"description\":\"This is the first item in the data list.\",\"quantity\":10,\"price\":19.99},{\"id\":2,\"name\":\"Item 2\",\"description\":\"This is the second item in the data list.\",\"quantity\":5,\"price\":24.99},{\"id\":3,\"name\":\"Item 3\",\"description\":\"This is the third item in the data list.\",\"quantity\":3,\"price\":34.99}]},\"section3\":{\"heading\":\"Users\",\"users\":[{\"id\":101,\"name\":\"John Doe\",\"email\":\"john@example.com\",\"age\":30},{\"id\":102,\"name\":\"Jane Smith\",\"email\":\"jane@example.com\",\"age\":25},{\"id\":103,\"name\":\"Mike Johnson\",\"email\":\"mike@example.com\",\"age\":40},{\"id\":101,\"name\":\"John Doe\",\"email\":\"john@example.com\",\"age\":30},{\"id\":102,\"name\":\"Jane Smith\",\"email\":\"jane@example.com\",\"age\":25},{\"id\":103,\"name\":\"Mike Johnson\",\"email\":\"mike@example.com\",\"age\":40}]}}},{\"header\":{\"title\":\"Random JSON Data\",\"date\":\"2023-07-25\",\"author\":\"ChatGPT\"},\"content\":{\"section1\":{\"heading\":\"Introduction\",\"text\":\"This is a randomly generated JSON file with a size of approximately 5KB. It's intended for demonstration and testing purposes only.\"},\"section2\":{\"heading\":\"Data\",\"items\":[{\"id\":1,\"name\":\"Item 1\",\"description\":\"This is the first item in the data list.\",\"quantity\":10,\"price\":19.99},{\"id\":2,\"name\":\"Item 2\",\"description\":\"This is the second item in the data list.\",\"quantity\":5,\"price\":24.99},{\"id\":3,\"name\":\"Item 3\",\"description\":\"This is the third item in the data list.\",\"quantity\":3,\"price\":34.99}]},\"section3\":{\"heading\":\"Users\",\"users\":[{\"id\":101,\"name\":\"John Doe\",\"email\":\"john@example.com\",\"age\":30},{\"id\":102,\"name\":\"Jane Smith\",\"email\":\"jane@example.com\",\"age\":25},{\"id\":103,\"name\":\"Mike Johnson\",\"email\":\"mike@example.com\",\"age\":40},{\"id\":101,\"name\":\"John Doe\",\"email\":\"john@example.com\",\"age\":30},{\"id\":102,\"name\":\"Jane Smith\",\"email\":\"jane@example.com\",\"age\":25},{\"id\":103,\"name\":\"Mike Johnson\",\"email\":\"mike@example.com\",\"age\":40}]}}},{\"header\":{\"title\":\"Random JSON Data\",\"date\":\"2023-07-25\",\"author\":\"ChatGPT\"},\"content\":{\"section1\":{\"heading\":\"Introduction\",\"text\":\"This is a randomly generated JSON file with a size of approximately 5KB. It's intended for demonstration and testing purposes only.\"},\"section2\":{\"heading\":\"Data\",\"items\":[{\"id\":1,\"name\":\"Item 1\",\"description\":\"This is the first item in the data list.\",\"quantity\":10,\"price\":19.99},{\"id\":2,\"name\":\"Item 2\",\"description\":\"This is the second item in the data list.\",\"quantity\":5,\"price\":24.99},{\"id\":3,\"name\":\"Item 3\",\"description\":\"This is the third item in the data list.\",\"quantity\":3,\"price\":34.99}]},\"section3\":{\"heading\":\"Users\",\"users\":[{\"id\":101,\"name\":\"John Doe\",\"email\":\"john@example.com\",\"age\":30},{\"id\":102,\"name\":\"Jane Smith\",\"email\":\"jane@example.com\",\"age\":25},{\"id\":103,\"name\":\"Mike Johnson\",\"email\":\"mike@example.com\",\"age\":40},{\"id\":101,\"name\":\"John Doe\",\"email\":\"john@example.com\",\"age\":30},{\"id\":102,\"name\":\"Jane Smith\",\"email\":\"jane@example.com\",\"age\":25},{\"id\":103,\"name\":\"Mike Johnson\",\"email\":\"mike@example.com\",\"age\":40}]}}}]";
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
@@ -27,11 +40,21 @@ public class InventoryController {
     @GetMapping("/all")
     @ResponseStatus(HttpStatus.OK)
     public List<InventoryDetailsResponse> getAllInventory(@RequestParam(value = "rndon", required = false) boolean rndOn,
+                                                          @RequestParam(value = "rndmemon", required = false) boolean rndMemOn,
             @RequestParam(value = "rndlimit", required = false, defaultValue = "0") int rndLimit){
         List<InventoryDetailsResponse> response = inventoryService.getAll();
         if(rndOn) {
             generateNumbers(rndLimit);
             System.out.println("random numbers generated " + rndLimit);
+        }
+
+        if(rndMemOn) {
+            try {
+                List<JsonNode> listJsonNodes = loadJson(rndLimit);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("random jsons generated " + rndLimit);
         }
 
         return response;
@@ -89,4 +112,36 @@ public class InventoryController {
             Math.random();
         }
     }
+
+    private List<JsonNode> loadJson(int n) throws JsonProcessingException {
+        List<JsonNode> listJsonNodes = new ArrayList<>();
+        for(int i = 0;i < n; i++){
+            listJsonNodes.add(parseJson());
+        }
+
+        return listJsonNodes;
+    }
+
+    private JsonNode parseJson() throws JsonProcessingException {
+        JsonNode jsonNode = OBJECT_MAPPER.readTree(JSONARRAY_STRING);
+        return jsonNode;
+    }
+
+//    public void loadFileFromResources() {
+//        // Specify the path to the file inside the resources folder
+//        String filePath = "hello.json";
+//
+//        // Load the resource using ClassPathResource
+//        ClassPathResource resource = new ClassPathResource(filePath);
+//
+//        // Perform operations on the resource, e.g., read the contents of the file
+//        try (InputStream inputStream = resource.getInputStream();
+//             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+//            String line;
+//            while ((line = reader.readLine()) != null) {
+//                // Process each line of the file
+//                System.out.println(line);
+//            }
+//        }
+//    }
 }
