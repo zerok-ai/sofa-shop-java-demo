59b6f1a42d817dad3e37963a9a66954748e7f578
Santhosh J <ssjdexucyche@gmail.com>
2023-08-25 13:54:31 +0530

    updated k8s folder

--- frontend/docker_push.sh
+++ frontend/docker_push.sh
@@ -2,6 +2,8 @@ DEFAULT_LABEL="0.0.1"
 LABEL="${1:-$DEFAULT_LABEL}"
 echo "Labeling: $LABEL"
 
-npm run build
+yarn build
 docker build . -t us-west1-docker.pkg.dev/zerok-dev/sofa-shop/frontend:$LABEL
 docker push us-west1-docker.pkg.dev/zerok-dev/sofa-shop/frontend:$LABEL
+
+kubectl apply -k ./k8s
