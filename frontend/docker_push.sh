f77f32e3be7ee696bbccd00142a45b13ca6c7112
Shivam Nagar <shivam.nagar@outlook.com>
2023-08-31 14:26:11 +0530

    reverted change

--- frontend/docker_push.sh
+++ frontend/docker_push.sh
@@ -3,7 +3,7 @@ LABEL="${1:-$DEFAULT_LABEL}"
 echo "Labeling: $LABEL"
 
 npm run build
-docker build . -t us-west1-docker.pkg.dev/zerok-dev/java-spring-boot-demo/frontend:$LABEL
-docker push us-west1-docker.pkg.dev/zerok-dev/java-spring-boot-demo/frontend:$LABEL
+docker build . -t us-west1-docker.pkg.dev/zerok-dev/sofa-shop/frontend:$LABEL
+docker push us-west1-docker.pkg.dev/zerok-dev/sofa-shop/frontend:$LABEL
 
 # kubectl apply -k ./k8s
